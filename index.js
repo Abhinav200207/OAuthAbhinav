405410bb35c08d66d2357d7c1cc1bd2a7fb6bc03
Abhinav <abhinav.maurya202@gmail.com>
2022-02-12 15:45:18 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,55 @@
+const express = require('express');
+const session = require('express-session');
+const passport = require('passport');
+require('./auth');
+
+const app = express();
+app.use(express.json());
+
+function isLoggedIn(req, res, next) {
+    req.user ? next() : res.sendStatus(401);
+}
+
+app.use(session({ secret: 'cats', resave: false, saveUninitialized: true }));
+app.use(passport.initialize());
+app.use(passport.session());
+
+app.get('/', (req, res) => {
+    res.send('<a href="/auth/google">Authenticate with Google</a>');
+});
+
+app.get('/auth/google',
+    passport.authenticate('google', { scope: ['email', 'profile'] }
+    ));
+
+app.get('/auth/google/callback',
+    passport.authenticate('google', {
+        successRedirect: '/protected',
+        failureRedirect: '/auth/google/failure'
+    })
+);
+
+app.get('/protected', isLoggedIn, (req, res) => {
+    res.send(`Hello ${req.user.displayName}`);
+});
+
+app.get('/protected1', isLoggedIn, (req, res) => {
+    console.log(`${req.user.displayName}`)
+    res.send(`<h1>${req.user.displayName}</h1>`)
+});
+
+app.get('/protected2', isLoggedIn, (req, res) => {
+    res.json(req.user);
+});
+
+app.get('/logout', (req, res) => {
+    req.logout();
+    req.session.destroy();
+    res.send('Goodbye!');
+});
+
+app.get('/auth/google/failure', (req, res) => {
+    res.send('Failed to authenticate..');
+});
+
+app.listen(5000, () => console.log('listening on port: 5000'));
